f6858f99585dd85be2a67d905e4934e0ceafc512
Chloe <chloepeng331@icloud.com>
2022-06-27 20:45:32 -0400

    228

---
+++ 228-summary-ranges.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+// var summaryRanges = function(nums) {
+    
+// };
+
+const summaryRanges = nums => {
+    let result = [];
+    
+    for (let start = 0, end = 0; end < nums.length; end++, start = end) {
+        while (nums[end] === nums[end+1]-1)
+            end++;
+        if (end !== start)
+            result.push (nums[start] + "->" + nums[end])
+        else result.push (""+ nums[end]);
+    }
+    return result;
+}
+
+// Time complexity: O(n)
+// Space complexity: O(1)
