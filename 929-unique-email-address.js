fc637e679f4da1ada2c4abc51342430623c2993e
Chloe <chloepeng331@icloud.com>
2022-06-28 23:41:32 -0400

    comments

---
+++ 929-unique-email-address.js
@@ -0,0 +1,8 @@
+// create a new set
+// loop through emails
+//     create a variable that split local and domain by "@"
+//     remove "."
+//     split domain by "+"
+//     create a newEmail variable to hold newLocal + '@' + domain
+//     add newEmail to set
+// return the size of the set
