f6ca206125d276054500121eceb367f33f8712d2
Chloe <chloepeng331@icloud.com>
2022-06-29 00:00:23 -0400

    email

--- 929-unique-email-address.js
+++ 929-unique-email-address.js
@@ -1,8 +1,24 @@
-// create a new set
-// loop through emails
-//     create a variable that split local and domain by "@"
-//     remove "."
-//     split domain by "+"
-//     create a newEmail variable to hold newLocal + '@' + domain
-//     add newEmail to set
-// return the size of the set
+/**
+ * @param {string[]} emails
+ * @return {number}
+ */
+ var numUniqueEmails = function(emails) {
+    let emailSet = new Set ();
+    
+    for (let i = 0; i < emails.length; i++) {
+        let [local, domain] = emails[i].split("@");
+        local = local.replace(/\./g, "").split("+");
+        let uniqueEmail = local[0] + "@" + domain;
+        emailSet.add(uniqueEmail);
+    }
+    return emailSet.size;
+};
+
+// 1. create emailSet
+// 2. loop through emails
+//     create a variable local and domain and sepaate them by @
+//     delete "." from the local
+//     split local from "+"
+//     create a uniqueEmail variable to hold local + '@' + domain
+//     add uniqueEmail to set
+// 3. return the size the set
