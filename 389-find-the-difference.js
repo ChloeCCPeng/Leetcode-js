890130049fee7fb6d450a8009693679cadf3c0e8
Chloe <chloepeng331@icloud.com>
2022-06-30 22:30:57 -0400

    389

--- 389-find-the-difference.js
+++ 389-find-the-difference.js
@@ -1,19 +1,19 @@
 var findTheDifference = function(s, t) {
-    let stack = {}
+    let stack = {};
     
     for (let char of s) {
         if (stack[char] === undefined) {
-            stack[char] = 1
+            stack[char] = 1;
         } else {
-            stack[char]++;
+            stack[char] ++;
         }
     }
     
     for (let char of t) {
         if (stack[char] !== undefined && stack[char] > 0) {
-            stack[char]--;
+            stack[char] --;
         } else {
-            return char;
+            return char
         }
     }
 };
