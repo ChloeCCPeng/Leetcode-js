c037cd47c3330b18193dc3ace1e02fb29e4c347c
Chloe <chloepeng331@icloud.com>
2022-07-03 14:33:25 -0400

    cleaner code

--- 415-add-string.js
+++ 415-add-string.js
@@ -1,26 +1,28 @@
- var addStrings = function(num1, num2) {
+var addStrings = function(num1, num2) {
     let i = num1.length -1;
     let j = num2.length -1;
     let result = [];
     let carry = 0;
+    
+    function carryUp (sum, remainder){
+        if (sum > 9) carry = 1;
+        else carry = 0;
+        result.push(remainder);
+    }
     
     while (i >= 0) {
         if (j >= 0) {
             let sum = Number(num1[i]) + Number(num2[j]) + carry;
             let remainder = sum % 10;
             
-            if (sum > 9) carry = 1;
-            else carry = 0;
-            result.push(remainder);
+            carryUp (sum, remainder)
             i--;
             j--;
         } else {
             let sum = Number(num1[i]) + carry;
             let remainder = sum % 10;
             
-            if (sum > 9) carry = 1;
-            else carry = 0;
-            result.push(remainder);
+            carryUp (sum, remainder)
             i--;
         }
     }
@@ -28,9 +30,7 @@
         let sum = Number(num2[j]) + carry;
         let remainder = sum % 10;
 
-        if (sum > 9) carry = 1;
-        else carry = 0;
-        result.push(remainder);
+        carryUp (sum, remainder)
         j--;
     }
     if (carry) result.push(carry)
