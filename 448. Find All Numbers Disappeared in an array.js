c41ba664b670df152d435892ee36185023021940
Chloe <chloepeng331@icloud.com>
2022-07-05 21:25:37 -0400

    448

---
+++ 448. Find All Numbers Disappeared in an array.js
@@ -0,0 +1,18 @@
+
+ var findDisappearedNumbers = function (nums) {
+
+    let result = [];
+    let nums2 = [];
+    nums2.length = nums.length
+    
+    for ( let i = 0; i < nums.length; i++) {
+        nums2[nums[i] -1] = nums[i]
+        }
+        for (let i = 0; i < nums2.length; i++) {
+            if (!nums2[i]) {
+                result.push(i+1)
+            }
+        }
+    return result
+};
+
