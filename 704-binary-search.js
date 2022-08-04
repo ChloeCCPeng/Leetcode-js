49e454d0f38b40faa7dba9055b1a9bd681391e0f
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-08-03 22:39:00 -0400

    Update 704-binary-search.js

--- 704-binary-search.js
+++ 704-binary-search.js
@@ -1,35 +1,36 @@
-// /**
-//  * @param {number[]} nums
-//  * @param {number} target
-//  * @return {number}
-//  */
-// var search = function(nums, target) {
-//     let left = 0;
-//     let right = nums.length-1;
-//     let middle = (right - left) / 2;
-    
-//     if (middle == target) {
-//         return nums[middle]
-//         } else if (target > middle) {
-//             left = nums[middle]
-//         } else if (target < middle) {
-//             right = nums[middle]
-//         }
-//     return nums[middle]
-// };
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
 var search = function(nums, target) {
-let first = 0
-let last = nums.length - 1
-
-while(first <= last){
-    let mid = Math.floor(((first + last) / 2))
-    if(nums[mid] < target){
-        first = mid + 1
-    }else if(nums[mid] > target){
-        last = mid - 1
-    } else{
-        return mid
-    }
-}
-return - 1
+    let left = 0;
+    let right = nums.length-1;
+    let middle = Math.floor((left + right) / 2))
+    
+    while (target != middle) {
+        return nums[middle]
+        } if (target > middle) {
+            left = nums[middle]
+        } else if (target < middle) {
+            right = nums[middle]
+        }
+    return nums[middle]
 };
+
+// var search = function(nums, target) {
+// let first = 0
+// let last = nums.length - 1
+
+// while(first <= last){
+//     let mid = Math.floor(((first + last) / 2))
+//     if(nums[mid] < target){
+//         first = mid + 1
+//     }else if(nums[mid] > target){
+//         last = mid - 1
+//     } else{
+//         return mid
+//     }
+// }
+// return - 1
+// };
