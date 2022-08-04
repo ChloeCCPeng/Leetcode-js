6c53092392ea74b966553eeee5faaf46675766b3
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-08-03 23:15:20 -0400

    Update 704-binary-search.js

--- 704-binary-search.js
+++ 704-binary-search.js
@@ -1,17 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
 var search = function(nums, target) {
-    var lo = 0,
-        hi = nums.length - 1,
-        mid;
-
-    while (lo <= hi) {
-        mid = Math.floor((lo + hi) / 2);
-        if (nums[mid] == target)
-            return mid;
-        if (nums[mid] < target)
-            lo = mid + 1;
-        else
-            hi = mid - 1;
+    let left = 0;
+    let right = nums.length-1;
+    
+    while (left <= right) {
+    let middle = Math.floor(((left + right) / 2))
+        
+        if (target > nums[middle]) {
+            left = middle + 1
+        } else if (target < nums[middle]) {
+            right = middle -1
+        } else {
+            return middle            
+        }
     }
-
-    return -1;
+    // return -1
 };
