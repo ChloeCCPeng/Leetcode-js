1b2933e8acdb6c02788b5959f70d9d36ade0c410
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-08-05 23:25:54 -0400

    Update 278-first-bad-version.js

--- 278-first-bad-version.js
+++ 278-first-bad-version.js
@@ -14,3 +14,22 @@ var solution = function(isBadVersion) {
         }
     };
 };
+
+
+var solution = function(isBadVersion) {
+    return function(n) {
+      let r = n;
+      let l = 0;
+      while(l <= r){
+        let mid = Math.floor((l+r)/2)
+        let version = isBadVersion(mid);
+        if(version !== isBadVersion(mid+1)){
+           return mid+1
+        }else if(version === false){
+          l = mid+1
+        }else{
+          r = mid-1
+        }
+      }
+    };
+};
