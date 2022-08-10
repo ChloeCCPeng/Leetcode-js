1e2fa1468828204140fb9a990f43f103092b27cd
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-08-09 23:31:05 -0400

    Update 35-Search-Insert-Position.js

--- 35-Search-Insert-Position.js
+++ 35-Search-Insert-Position.js
@@ -7,7 +7,7 @@ var searchInsert = function(nums, target) {
     let left = 0, right = nums.length -1
     while (left <= right) {
         let mid = Math.floor((left + right)/2)
-        if (target = nums[mid]) {
+        if (target == nums[mid]) {
             return mid
         } else if (target < nums[mid]) {
             right = mid - 1
@@ -17,21 +17,3 @@ var searchInsert = function(nums, target) {
     }
     return left
 };
-
-
-// var searchInsert = function(nums, target) {
-//     var right = nums.length - 1;
-//     var left = 0;
-//     while (left < right) {
-//         var mid = Math.floor((left + right) / 2);
-//         if (target === nums[mid]) {
-//             return mid;
-//         } else if (target < nums[mid]) {
-//             right = mid - 1;
-//         } else {
-//             left = mid + 1
-//         }
-//     }
-    
-//     return target > nums[left] ? left + 1 : left;
-// };
