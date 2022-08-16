c807229bc89e5196f0662ccd9485fed78063beb3
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-08-15 23:19:46 -0400

    Update 977-squares-of-sorted-array.js

--- 977-squares-of-sorted-array.js
+++ 977-squares-of-sorted-array.js
@@ -1,21 +1,30 @@
 
-var sortedSquares = function(nums) {
-    // use two pointers
-    // create a new array
-    const result = new Array(nums.length);
-    let left = 0, 
-        right = nums.length - 1;
+// var sortedSquares = function(nums) {
+//     // use two pointers
+//     // create a new array
+//     const result = new Array(nums.length);
+//     let left = 0, 
+//         right = nums.length - 1;
     
-	// don't want to rearrange the array, so iterating the array in descending order 
-    for (let i = nums.length - 1; i >= 0; i--) {
-        if (Math.abs(nums[left]) < Math.abs(nums[right])) {
-            result[i] = nums[right] ** 2
-            right--;
-        } else {
-            result[i] = nums[left] ** 2
-            left++;
-        }
-    }
+// 	// don't want to rearrange the array, so iterating the array in descending order 
+//     for (let i = nums.length - 1; i >= 0; i--) {
+//         if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+//             result[i] = nums[right] ** 2
+//             right--;
+//         } else {
+//             result[i] = nums[left] ** 2
+//             left++;
+//         }
+//     }
     
-    return result;
+//     return result;
+// };
+
+
+
+var sortedSquares = function (A) {
+	A = A.map(x => x * x);
+	A.sort((a, b) => a - b);
+
+	return A;
 };
