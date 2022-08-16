7c49644f3e378e933f14b2666bd85fd58e9e1eb4
Chloe Peng <61214246+ChloeCCPeng@users.noreply.github.com>
2022-08-15 23:18:37 -0400

    Update 977-squares-of-sorted-array.js

--- 977-squares-of-sorted-array.js
+++ 977-squares-of-sorted-array.js
@@ -16,3 +16,25 @@ var sortedSquares = function(A) {
   }
   return res;
 };
+
+
+var sortedSquares = function(nums) {
+    // use two pointers
+    // create a new array
+    const result = new Array(nums.length);
+    let left = 0, 
+        right = nums.length - 1;
+    
+	// don't want to rearrange the array, so iterating the array in descending order 
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+            result[i] = nums[right] ** 2
+            right--;
+        } else {
+            result[i] = nums[left] ** 2
+            left++;
+        }
+    }
+    
+    return result;
+};
